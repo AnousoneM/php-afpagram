867e569bd380ccd03be40b75e14d0aff21792a2b
AnousoneM <anousone.mounivongs@gmail.com>
2025-03-04 23:46:14 +0100

    mise à jour de la page de confirmation

--- src/View/view-confirmation.php
+++ src/View/view-confirmation.php
@@ -2,9 +2,21 @@
 
 <body class="bg-primary">
 
-    <div class="container">
-        <h1 class="text-center my-2 text-white">Inscription prise en compte</h1>
-        <p class="text-center">Vous pouvez maintenant vous connecter : <a href="../Controller/controller-connexion.php" class="text-dark fs-5">Connexion</a></p>
+    <h1 class="text-center my-2 text-white">Inscription prise en compte !</h1>
+
+    <div class="col-lg-4 col-12 mx-auto my-3 bg-light p-4 shadow rounded">
+
+        <div class="row g-1 justify-content-center">
+            <div class="col-lg-12 text-center">
+                <p class="fs-5">Vous pouvez maintenant vous connecter</p>
+            </div>
+
+            <div class="col-lg-12">
+                <a class="btn btn-primary w-100" href="../Controller/controller-connexion.php">Connexion</a>
+            </div>
+
+        </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
